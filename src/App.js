70edd33cf4f07658a7b73acfe186e16eec7f6101
simminseon <alstjs7440@naver.com>
2022-12-16 17:55:41 +0900

    회원가입 - useContext 적용, 에러케이스 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { createContext, useReducer } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Container from "./component/layout/Container";
 import Header from "./component/layout/Header";
@@ -9,14 +9,48 @@ import BlogPost from "./component/page/blog/BlogPost";
 import Login from "./component/page/login/Login";
 import Join from "./component/page/join/Join";
 
+const initialState = {
+  memberData: [],
+};
+export const MemberContext = createContext({
+  memberData: [],
+  dispatch: () => {},
+});
+
+export const ADD_MEMBER = "ADD_MEMBER";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_MEMBER: {
+      // const memberData = [
+      //   {
+      //     id: action.id,
+      //     password: action.password,
+      //     email: action.email,
+      //   },
+      // ];
+      const memberData = action.memberData;
+      return {
+        ...state,
+        memberData,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state.memberData);
   return (
-    <>
+    <MemberContext.Provider value={{ memberData: state.memberData, dispatch }}>
       <Header>소플의 미니 블로그</Header>
       <Container>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
-            <Route path="/" element={<BlogList />} />
+            {/* <Route path="/" element={<BlogList />} /> */}
             <Route path="blogWrite" element={<BlogWrite />} />
             <Route path="blogPost/:post" element={<BlogPost />} />
             <Route path="login" element={<Login />} />
@@ -25,7 +59,7 @@ function App() {
         {/* <Login /> */}
         <Join />
       </Container>
-    </>
+    </MemberContext.Provider>
   );
 }
 
--- src/component/page/join/Join.jsx
+++ src/component/page/join/Join.jsx
@@ -1,32 +1,93 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import React, { useContext, useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
 import Button from "../../ui/Button";
 import Input from "../../ui/Input";
 import Label from "../../ui/Label";
 import { useInput } from "../../../hooks/useInput";
 import ErrorMessage from "../../ui/ErrorMessage";
+import { MemberContext, ADD_MEMBER } from "../../../App";
 
 const StyledField = styled.div`
   padding-bottom: 20px;
 `;
 
 function Join() {
+  const { memberData, dispatch } = useContext(MemberContext);
+
   const [id, onChangeId] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [email, onChangeEmail] = useInput("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [passwordCheckError, setPasswordCheckError] = useState(false);
+  const [idError, setIdError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const [member, setMember] = useState({
+    id,
+    password,
+    email,
+  });
+  // console.log(memberData);
 
   useEffect(() => {
-    if (password !== passwordCheck && passwordCheck.length > 0) {
+    if (password !== passwordCheck) {
       setPasswordCheckError(true);
-    } else if (password === passwordCheck) {
+    } else {
       setPasswordCheckError(false);
     }
   }, [password, passwordCheck]);
 
+  // const checkValue = (id, password, email) => {
+  //   if (id.length <= 0) {
+  //     return setIdError(true);
+  //   }
+  //   // else {
+  //   //   setIdError(false);
+  //   // }
+  //   if (password.length <= 0) {
+  //     return setPasswordError(true);
+  //   }
+  //   // else {
+  //   //   setPasswordError(false);
+  //   // }
+  //   if (email.length <= 0) {
+  //     return setEmailError(true);
+  //   }
+  //   // else {
+  //   //   setEmailError(false);
+  //   // }
+
+  //   return true;
+  // };
+
   const onSubmitJoin = (e) => {
     e.preventDefault();
+
+    if (id.length <= 0) {
+      setIdError(true);
+    } else {
+      setIdError(false);
+    }
+    if (password.length <= 0) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+    if (email.length <= 0) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+
+    // if (!checkValue(id, password, email)) {
+    //   return;
+    // }
+
+    dispatch({
+      type: ADD_MEMBER,
+      memberData: [...memberData, { id: id, password: password, email: email }],
+    });
   };
 
   const onChangePasswordCheck = (e) => {
@@ -44,7 +105,7 @@ function Join() {
           onChange={onChangeId}
           placeholder={"아이디를 입력해주세요"}
         />
-        {<ErrorMessage>아이디를 입력해주세요.</ErrorMessage>}
+        {idError && <ErrorMessage>아이디를 입력해주세요.</ErrorMessage>}
       </StyledField>
       <StyledField>
         <Label id={"inpPassword"}>비밀번호</Label>
@@ -55,13 +116,13 @@ function Join() {
           onChange={onChangePassword}
           placeholder={"비밀번호를 입력해주세요"}
         />
-        {/* {error && <ErrorMessage>비밀번호를 입력해주세요.</ErrorMessage>} */}
+        {passwordError && <ErrorMessage>비밀번호를 입력해주세요.</ErrorMessage>}
       </StyledField>
       <StyledField>
-        <Label id={"inpPasswordConfirm"}>비밀번호 확인</Label>
+        <Label id={"inpPasswordCheck"}>비밀번호 확인</Label>
         <Input
-          type={"passwordConfirm"}
-          id={"inpPasswordConfirm"}
+          type={"password"}
+          id={"inpPasswordCheck"}
           value={passwordCheck}
           onChange={onChangePasswordCheck}
           placeholder={"비밀번호를 한번 더 입력해주세요"}
@@ -70,6 +131,7 @@ function Join() {
         {passwordCheckError && (
           <ErrorMessage>비밀번호가 다릅니다.</ErrorMessage>
         )}
+        {/* {passwordError && <ErrorMessage>비밀번호를 입력해주세요.</ErrorMessage>} */}
       </StyledField>
       <StyledField>
         <Label id={"inpEmail"}>이메일</Label>
@@ -80,7 +142,7 @@ function Join() {
           onChange={onChangeEmail}
           placeholder={"이메일을 입력해주세요"}
         />
-        {/* {error && <ErrorMessage>이메일을 입력해주세요.</ErrorMessage>} */}
+        {emailError && <ErrorMessage>이메일을 입력해주세요.</ErrorMessage>}
       </StyledField>
       <Button size={"full"}>회원가입</Button>
     </form>
